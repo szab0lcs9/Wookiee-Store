0f716606b517d4b2a783c9787dca354cde7340c5
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-01-09 21:24:34 +0100

    created registration form

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/product/{product}', [Controllers\ProductController::class, 'show'])->name('product.show');
 
 Route::get('/register', [Controllers\Auth\RegisterController::class, 'create'])->name('auth.register');
+Route::post('/register', [Controllers\Auth\RegisterController::class, 'store']);
 
 Route::get('/new-product', [Controllers\ProductController::class, 'create'])->name('product.create');
 Route::post('/new-product', [Controllers\ProductController::class, 'store']);
