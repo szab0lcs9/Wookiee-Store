325d32080d6a1827476e2f6c414037e8bd7070b0
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-01-10 20:24:31 +0100

    updated web routes

--- routes/web.php
+++ routes/web.php
@@ -21,15 +21,23 @@ Route::get('/', [Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/product/{product}', [Controllers\ProductController::class, 'show'])->name('product.show');
 
-Route::get('/register', [Controllers\Auth\RegisterController::class, 'create'])->name('auth.register');
-Route::post('/register', [Controllers\Auth\RegisterController::class, 'store']);
+Route::middleware(['guest'])->group(function() {
 
-Route::get('/login', [Controllers\Auth\LoginController::class, 'create'])->name('auth.login');
-Route::post('/login', [Controllers\Auth\LoginController::class, 'store']);
-
-Route::post('/logout', [Controllers\Auth\LoginController::class, 'destroy'])->name('auth.logout');
-
-Route::get('/new-product', [Controllers\ProductController::class, 'create'])->name('product.create');
-Route::post('/new-product', [Controllers\ProductController::class, 'store']);
+    Route::get('/register', [Controllers\Auth\RegisterController::class, 'create'])->name('auth.register');
+    Route::post('/register', [Controllers\Auth\RegisterController::class, 'store']);
+    
+    Route::get('/login', [Controllers\Auth\LoginController::class, 'create'])->name('auth.login');
+    Route::post('/login', [Controllers\Auth\LoginController::class, 'store']);
+    
+});
 
 Route::get('/products', [Controllers\ProductController::class, 'showAll'])->name('product.total');
+
+Route::middleware(['auth'])->group(function() {
+
+    Route::get('/new-product', [Controllers\ProductController::class, 'create'])->name('product.create');
+    Route::post('/new-product', [Controllers\ProductController::class, 'store']);
+    
+    Route::post('/logout', [Controllers\Auth\LoginController::class, 'destroy'])->name('auth.logout');
+
+});
