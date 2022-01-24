c8cf402aeea96157cc43a817f402f89408c2693b
Szabolcs Veres <73792909+szab0lcs9@users.noreply.github.com>
2022-01-24 17:57:47 +0100

    added title to the header

--- resources/views/layout/_header.blade.php
+++ resources/views/layout/_header.blade.php
@@ -54,8 +54,8 @@
         <header class="bg-dark bg-gradient py-5">
             <div class="container px-4 px-lg-5 my-5">
                 <div class="text-center text-white">
-                    <h1 class="display-4 fw-bolder"></h1>
-                    <p class="lead fw-normal text-white-50 mb-0"></p>
+                    <h1 class="display-4 fw-bolder">{{ __('Wookiee Store') }}</h1>
+                    <p class="lead fw-normal text-white-50 mb-0"> {{ __("Chewbacca's favorite online shop.") }}</p>
                 </div>
             </div>
         </header>
